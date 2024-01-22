30e41318bad39ff4aaa5b12a02e88cdb7c55cc0f
hxrda <henein.roda@gmail.com>
2024-01-22 15:58:51 +0200

    Exercise complete

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	FlatList,
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	//States:
+
+	const [item, setItem] = useState("");
+	const [items, setItems] = useState([]);
+
+	//Functions:
+
+	const handleAdd = () => {
+		setItems([...items, item]);
+		setItem(""); //empty text input field
+		//console.log(items);
+	};
+
+	const clearAll = () => {
+		setItems([]);
+		//console.log(items);
+	};
+
+	//Components:
+
+	const listHeader = () => {
+		return (
+			<View>
+				<Text>Shopping List</Text>
+			</View>
+		);
+	};
+
+	//Rendering:
+	return (
+		<View style={styles.container}>
+			<View
+				style={{
+					flex: 1,
+				}}
+			>
+				<TextInput
+					placeholder="Type Item"
+					value={item}
+					onChangeText={(text) => setItem(text)}
+				/>
+
+				<View style={styles.buttons}>
+					<Button title="Add Item" onPress={handleAdd} />
+					<Button title="Clear" onPress={clearAll} />
+				</View>
+			</View>
+
+			<View style={{ flex: 3 }}>
+				<FlatList
+					data={items}
+					ListHeaderComponent={listHeader}
+					renderItem={({ item }) => (
+						<View style={styles.listItem}>
+							<Text style={{ fontSize: 14 }}>{item}</Text>
+						</View>
+					)}
+				/>
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		marginTop: 100,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttons: {
+		flexDirection: "row",
+	},
+	listItem: {
+		padding: 5,
+		width: 300,
+		alignItems: "center",
+	},
 });
